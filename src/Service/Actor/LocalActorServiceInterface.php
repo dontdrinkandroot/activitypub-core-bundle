32fd0683e537b7496bacfeedd759c0240947e8ff
Philip Washington Sorst <philip@sorst.net>
2023-10-27 13:37:59 +0200

    refactor: Force LocalActorServiceInterface to implement ObjectProviderInterface

--- src/Service/Actor/LocalActorServiceInterface.php
+++ src/Service/Actor/LocalActorServiceInterface.php
@@ -4,10 +4,12 @@ namespace Dontdrinkandroot\ActivityPubCoreBundle\Service\Actor;
 
 use Dontdrinkandroot\ActivityPubCoreBundle\Model\LocalActorInterface;
 use Dontdrinkandroot\ActivityPubCoreBundle\Model\SignKey;
+use Dontdrinkandroot\ActivityPubCoreBundle\Model\Type\CoreType;
 use Dontdrinkandroot\ActivityPubCoreBundle\Model\Type\Extended\Actor\Actor;
 use Dontdrinkandroot\ActivityPubCoreBundle\Model\Type\Property\Uri;
+use Dontdrinkandroot\ActivityPubCoreBundle\Service\Object\ObjectProviderInterface;
 
-interface LocalActorServiceInterface
+interface LocalActorServiceInterface extends ObjectProviderInterface
 {
     public function findLocalActorByUsername(string $username): ?LocalActorInterface;
 
@@ -21,4 +23,6 @@ interface LocalActorServiceInterface
     public function toActivityPubActor(LocalActorInterface $localActor): Actor;
 
     public function getSignKey(LocalActorInterface $localActor): SignKey;
+
+    public function provide(Uri $uri, ?SignKey $signKey): Actor|false|null;
 }
